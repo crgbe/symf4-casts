c61093bbc858b7775dc6bde22a12de6cc1ac3085
crgbe <credo.gbededji@gmail.com>
2019-12-11 11:48:24 +0100

    Installing libraries, bootstrap, font-awesome. Creating JSON API endpoint for like button on article show view. Testing out services operations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,17 +4,31 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/phpinfo")
+     */
+    public function phpinfo(){
+        echo phpinfo();
+        return new Response("These are PHP infos");
+    }
+
+    /**
+     * @Route("/", name="app_homepage")
      */
     public function homepage(){
-        return new Response('First page created');
+        $name = "This is the homepage";
+
+        return $this->render('article/homepage.html.twig', [
+            'title' => $name,
+        ]);
     }
 
     /**
@@ -23,13 +37,25 @@ class ArticleController extends AbstractController
     public function show($slug){
         $comments = [
             'You are so fast when coding now !!!',
-            'Remember at your beginning, you did not even knew what to write',
+            'Remember at your beginning, you did not even know what to write',
             'hhhh!!! You little kind boy :D'
         ];
 
         return $this->render('article/show.html.twig', [
             'title' => ucfirst(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleHeart($slug, LoggerInterface $logger){
+        $logger->info("The article is being loved");
+
+        return $this->json([
+            'hearts' => rand(5, 100),
         ]);
     }
 }
