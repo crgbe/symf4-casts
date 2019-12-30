32c35d620fd57b9cc61cd105ce73a65cd6ffedbd
crgbe <credo.gbededji@gmail.com>
2019-12-30 12:51:47 +0100

    Creating Twig extension (a filter specifically) with make:twig-extension command from MakerBundle

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    private $markdownHelper;
+
+    public function __construct(MarkdownHelper $markdownHelper)
+    {
+        $this->markdownHelper = $markdownHelper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('cached_markdown', [$this, 'markdownParser'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function markdownParser($value)
+    {
+        return $this->markdownHelper->parse($value);
+    }
+}
